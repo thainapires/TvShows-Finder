ba03b042ee4f7f349a36b3258a6510688c2e7693
Thainá Pires <thainaspiress@gmail.com>
2020-10-10 21:08:57 -0300

    Deleting howitworks.html

--- app.py
+++ app.py
@@ -12,10 +12,6 @@ def index():
 def about():
     return render_template('about.html', randn = str(random.randint(4001,5000)))
 
-@app.route('/howitworks') 
-def howitworks():
-    return render_template('howitworks.html', randn = str(random.randint(4001,5000)))
-
 @app.route('/search_tvshow', methods=['GET','POST'])
 def search_tvshow():
     search_term = ''
@@ -26,26 +22,36 @@ def search_tvshow():
     response = requests.get("http://api.tvmaze.com/search/shows?q={}".format(search_term))
     #jprint(response.json()[1])
     response = response.json()
+    #print(type(response))
     tvshow =[]
     if(not response):
         return render_template('movies.html', randn = str(random.randint(101,4000)))
-    for i in range(4):
-        img = response[i]["show"]["image"]["original"]
+    jprint(response)
+    for i in range(len(response)-1):
+        img = response[i]["show"]["image"]
         if(img == None):
-            img = "https://i.imgflip.com/1g778w.jpg"
+            img = "../static/img_404.jpg"
+        else:
+            img = img["original"]
         name = response[i]['show']['name']
+        if(name == None):
+            name = 'not found'
         year = response[i]['show']['premiered']
         if(year == None):
             year = '-'
         else:
             year = year[0:4]
         time = response[i]['show']['schedule']['time'][0:2]
+        if(time == None):
+            time = '0'
         genres = response[i]['show']['genres']
-        summary = response[i]['show']['summary'][0:110]+'...'
+        if(genres == None):
+            genres = 'not found'
+        summary = response[i]['show']['summary']
         if(summary == None):
             summary = '-'
         else:
-            summary = summary.replace('<p>', '').replace('</p>', '').replace('<b>', '').replace('</b>', '')
+            summary = summary[0:110]+'...'.replace('<p>', '').replace('</p>', '').replace('<b>', '').replace('</b>', '')
         website = response[i]['show']['officialSite']
         rating = response[i]['show']['rating']['average']
         tvshow.append([
